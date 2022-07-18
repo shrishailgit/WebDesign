397d12d94e22a0747645250212e018b32798accf
shrishail <104854541+shrishailgit@users.noreply.github.com>
2022-07-18 13:59:59 +0530

    Add files via upload

---
+++ user.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit } from '@angular/core';
+import { NgModule } from '@angular/core';
+@Component({
+  selector: 'app-user',
+  templateUrl: './user.component.html',
+  styleUrls: ['./user.component.scss']
+})
+export class UserComponent implements OnInit {
+  
+  
+  
+ // url="assets/images/myimge.jpg";
+  constructor() { }
+
+  ngOnInit(): void {
+    
+ //   console.log("hello");
+ 
+  
+
+  }
+
+
+  
+}
